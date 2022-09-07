9960aad79716b0ec2bbee504cf964a552f83eff5
Igor Moreira <igormsantos1@icloud.com>
2022-09-07 08:46:13 -0300

    atualizando dados

--- index.php
+++ index.php
@@ -1,29 +1,29 @@
     <?php include_once("templates/header.php") ?>
     
     <header class="header">
-    <img class="logo-img" src="img/logo.png" alt="Logo Balle Bot">
+    <img class="logo-img" src="img/logo4.png" alt="Logo G-Team">
     <nav class="nav-bar">
-        <a href="#" class="nav-bar-item">Comunidade Balerini</a>
-        <a href="#" class="nav-bar-item">Tutorial</a>
-        <a href="#" class="nav-bar-item">Open Source</a>
-        <a href="#" class="nav-bar-item">Comandos</a>
+        <a href="#" class="nav-bar-item">Comunidade G-Team</a>
+        <a href="#" class="nav-bar-item">Jogos</a>
+        <a href="#" class="nav-bar-item">Campeonatos</a>
+        <a href="#" class="nav-bar-item">Quem somos nós</a>
     </nav>
     </header>
 
     <main class="main-content">
         <section class="content1">
             <div class="content1-div">
-                <h1 class="content1-title">Balle Bot</h1>
-                <h2 class="content1-subtitle">Deixe a sua comunidade segura de um jeito facil</h2>
-                <button class="card-btn">Me adicione</button>
+                <h1 class="content1-title">G-Team</h1>
+                <h2 class="content1-subtitle">Venha fazer parte desse time</h2>
+                <button class="card-btn">Inscrever-se</button>
             </div>
-            <img class="logo-img2" src="img/logo2.png" alt="Imagem da logo">
+            <img class="logo-img2" src="img/logo3.png" alt="Imagem da logo">
         </section>
         
         
         
         <section class="content2">
-            <h3 class="content2-title">O que ela faz por você</h3>
+            <h3 class="content2-title">Venha ser um campeão você também!</h3>
             <p class="content2-subtitle">Cria um <b>sistema de verificação</b> capcha para novos membros garatirem que não são robôs.</p>
             <p class="content2-subtitle">Possui um <b>sistema de banimento</b> automático para links suspeitos que forem enviados na comunidade.</p>
             <p class="content2-subtitle"><b>Sistema de avisos e banimento manual</b>, para pessoas administradoras utilizarem</p>
